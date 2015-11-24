de789bd698b5cf2e04a123b75699a5f7ee54dd7b
Kyle Hoang <kyle.hoang@contemi.com.vn>
2015-11-24 14:08:22 +0700

    added misc view

---
+++ component/site/controllers/misc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_contact
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Controller for single demo view
+ *
+ * @since  1.5.19
+ */
+class DummyControllerMisc extends JControllerLegacy
+{
+}
---
+++ component/site/views/misc/view.html.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_contact
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * HTML Demo View class for the Dummy component
+ *
+ * @since  1.5
+ */
+class DummyViewMisc extends JViewLegacy
+{
+	public function display($tpl = null)
+	{
+		$app        = JFactory::getApplication();
+		$user       = JFactory::getUser();
+
+		return parent::display($tpl);
+	}
+}
